61995fdbbf2655c1f62afbb64b1bd1a9997818fe
jukitot <irzhyckaya@gmail.com>
2024-12-05 18:40:13 +0200

    hw-2

--- main.js
+++ main.js
@@ -1,75 +1,47 @@
-const http = require('node:http');
+
 const path = require('node:path')
-const readline = require("node:readline/promises");
 const fs = require('node:fs/promises')
 
 
 const foo = async () => {
-    //http module
-    // const server = http.createServer((req, res) => {
-    //     res.writeHead(200, {'Content-Type': 'application/json'});
-    //     const url = req.url;
-    //     if (url === '/about') {
-    //         switch (req.method) {
-    //             case  'GET':
-    //                 return res.end(JSON.stringify({
-    //                     data: 'About Page',
-    //                 }));
-    //             case 'POST' :
-    //                 return res.end(JSON.stringify({
-    //                     data: 'About Page',
-    //                 }));
-    //         }
-    //
-    //     }
-    //     if (url === '/contact') {
-    //         return res.end(JSON.stringify({
-    //             data: 'Contact Page',
-    //         }));
-    //     }
-    //     res.end(JSON.stringify({
-    //         data: 'Hello World!',
-    //     }));
-    // });
-    //
-    // server.listen(8000);
 
-    //path
-    // const pathToHelper = path.join(__dirname, 'helpers', 'helper.js')
-    // console.log(path.basename(pathToHelper));
-    // console.log(path.dirname(pathToHelper));
-    // console.log(path.extname(pathToHelper));
-    // console.log(path.parse(pathToHelper));
-    // console.log(path.normalize('D:/okten-study\\may-2024\\node-js\\NodeJS\\helpers\n'));
-    // console.log(path.isAbsolute('D:\okten-study\may-2024\node-js\NodeJS\helpers'));
+    await fs.mkdir(path.join(process.cwd(), 'baseFolder' ))
 
-    //readline
-    //
-    // const rlInstance = readline.createInterface({
-    //     input: process.stdin,
-    //     output: process.stdout
-    // })
-    // const name = await rlInstance.question('What is your name?');
-    // console.log(`Hello ${name}`)
-    // const age = await rlInstance.question('What is your age?');
-    // console.log(`You are ${age} years old`)
-    // // rlInstance.close();
-    // process.exit(1)
+    const dirs = ['folder1','folder2','folder3','folder4','folder5']
+    const files = ['file1.txt','file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
 
-    //fs
-    await fs.writeFile('test.txt', 'Hello World!')
-    // await fs.writeFile('test.json', JSON.stringify({name: 'Anastasiia', age: 21}, null, 2))
-    // await fs.mkdir('sdf/sdf')
-    const data = await fs.readFile(path.join(process.cwd(), 'test.txt'), 'utf-8')
-    console.log(data);
-    // const data2 = await fs.readFile(path.join(process.cwd(), 'test.json'), 'utf-8')
-    // console.log(data2);
-    //
-    // await fs.appendFile('test.txt', '\nhello2')
-    // await fs.rename('test.json', 'helpers/test2.json')
-    //
-    // await fs.copyFile('test.txt', 'test2.txt')
+    for (const dirName of dirs) {
+        const dirPath = path.join('baseFolder', dirName)
+        await fs.mkdir(dirPath)
+        for (const fileName of files) {
+            const filePath = path.join(dirPath, fileName)
+            await fs.writeFile(filePath, '', 'utf-8')
+        }
+    }
 
-    await fs.mkdir(path.join(process.cwd(), 'asd'))
+
+    async function logPaths(folderPath) {
+        const items = await fs.readdir(folderPath, { withFileTypes: true });
+
+        for (const item of items) {
+            const itemPath = path.join(folderPath, item.name);
+
+            if (item.isDirectory()) {
+                console.log(`Folder: ${itemPath}`);
+                await logPaths(itemPath);
+            } else if (item.isFile()) {
+                console.log(`File: ${itemPath}`);
+            }
+        }
+    }
+    await logPaths( path.join(process.cwd(), 'baseFolder'));
+
+
+
+
+
+
+
+
 }
 void foo();
