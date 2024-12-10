6889ce512a9893984eaf124eda17bc76fd0285b5
jukitot <irzhyckaya@gmail.com>
2024-12-10 11:24:09 +0200

    lesson-3

--- helpers/helper.js
+++
@@ -1,9 +0,0 @@
-console.log('__________helper.js___________')
-console.log(__dirname)
-console.log(__filename)
-
-const foo = () => {
-    console.log('WWW')
-}
-
-module.exports = {foo}
--- main.js
+++ main.js
@@ -1,22 +1,72 @@
-console.log('__________main.js___________')
-console.log(__dirname)
-console.log(__filename)
-console.log(process.cwd())
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
 
-const {foo} = require('./helpers/helper')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+let users = [
+    {id: 1, name: 'Maksym', email: 'feden@gmail.com', password: 'qwe123'},
+    {id: 2, name: 'Alina', email: 'alindosik@gmail.com', password: 'ert345'},
+    {id: 3, name: 'Anna', email: 'ann43@gmail.com', password: 'ghj393'},
+    {id: 4, name: 'Tamara', email: 'tomochka23@gmail.com', password: 'afs787'},
+    {id: 5, name: 'Dima', email: 'taper@gmail.com', password: 'rtt443'},
+    {id: 6, name: 'Rita', email: 'torpeda@gmail.com', password: 'vcx344'},
+    {id: 7, name: 'Denis', email: 'denchik@gmail.com', password: 'sdf555'},
+    {id: 8, name: 'Sergey', email: 'BigBoss@gmail.com', password: 'ccc322'},
+    {id: 9, name: 'Angela', email: 'lala@gmail.com', password: 'cdd343'},
+    {id: 10, name: 'Irina', email: 'irka7@gmail.com', password: 'kkk222'},
+];
 
-foo()
+//get-list-users -> users(GET)
 
-const readLine = require('node:readline')
-const foo1 = async () => {
-    const rlInstance = readLine.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-    rlInstance.question('What is your name?', (name)=>{
-        console.log(`Hello ${name}`);
-        rlInstance.close()
-    })
-}
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
+})
 
-void foo1();
+
+//create-users -> users(POST)
+
+app.post('/users', (req, res)=>{
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    users.push(newUser)
+    res.status(201).json(newUser)
+    }
+)
+
+//get-user-by-id -> users/:id(GET)
+
+app.get('/users/:userId', (req,res)=>{
+    // console.log('params: ', req.params)
+    // console.log('query: ', req.query)
+    // console.log('body: ', req.body)
+
+    const user = users.find(user=>user.id === Number(req.params.userId))
+    res.json(user)
+})
+
+//delete-user -> users/:id(DELETE)
+
+app.delete('/users/:userId' ,(req, res)=>{
+    users = users.filter(user=> user.id !== Number(req.params.userId));
+    res.sendStatus(204).json({message: 'User was deleted'})
+})
+//update-user -> users/:id(PUT)
+
+
+//CRUD
+// Create -> POST
+// Read -> GET
+// Update -> PUT
+// Delete -> DELETE
+
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`Server has been started on port ${port} `)
+})
